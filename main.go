5a4a87c525383a0634f3f61d51d2d90d22af828d
Julian Matschinske <julian.matschinske@bitspark.de>
2018-02-24 16:50:11 +0100

    Restructure

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-func runTests(dir string) {
+func runTests(dir string) (int, int, int) {
 	testFiles, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
@@ -17,10 +17,13 @@ func runTests(dir string) {
 
 	succsTotal := 0
 	failsTotal := 0
-	errors := false
-	files := 0
+	filesTotal := 0
 	for _, file := range testFiles {
 		if file.IsDir() {
+			succs, fails, files := runTests(dir + file.Name() + "/")
+			succsTotal += succs
+			failsTotal += fails
+			filesTotal += files
 			continue
 		}
 		if !strings.HasSuffix(file.Name(), "_test.yaml") {
@@ -33,7 +36,6 @@ func runTests(dir string) {
 		if succs, fails, err := slang.TestOperator(path, os.Stdout, false); err != nil || succs == 0 || fails != 0 {
 			if err != nil {
 				log.Fatal(err)
-				errors = true
 			}
 			succsTotal += succs
 			failsTotal += fails
@@ -41,30 +43,29 @@ func runTests(dir string) {
 			succsTotal += succs
 			failsTotal += fails
 		}
-		files++
+		filesTotal++
 		fmt.Println()
 	}
+
+	return succsTotal, failsTotal, filesTotal
+}
+
+func main() {
+	succsTotal, failsTotal, filesTotal := runTests("./slang/")
 
 	fmt.Println()
 	fmt.Println()
 	fmt.Println("SUMMARY OVER ALL TEST FILES")
 	fmt.Println()
-	fmt.Printf("Files: %3d\n", files)
+	fmt.Printf("Files: %3d\n", filesTotal)
 	fmt.Printf("Tests: %3d / %3d\n", succsTotal, succsTotal + failsTotal)
 	fmt.Println()
 
-	if failsTotal == 0 && !errors {
+	if failsTotal == 0 {
 		fmt.Println("ALL TESTS PASSED :)")
 		os.Exit(0)
-	} else if failsTotal != 0 {
+	} else {
 		fmt.Printf("%d TEST(S) FAILED :(\n", failsTotal)
 		os.Exit(-1)
-	} else {
-		fmt.Printf("ERRORS OCCURRED :(\n")
-		os.Exit(-1)
 	}
-}
-
-func main() {
-	runTests("./slang/")
 }
