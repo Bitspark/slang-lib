2cdc8e726c14e87625e14bd0f2a35cc70948f1b8
Julian Matschinske <julian.matschinske@bitspark.de>
2018-07-21 02:34:08 +0200

    Add CircleCI

---
+++ ci/package.py
@@ -0,0 +1,20 @@
+import sys
+from utils import execute_commands
+
+name = 'PACKAGE'
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python3 package.py vx.y.z')
+        exit(-1)
+
+    versioned_dist = 'slang-lib-' + sys.argv[1].replace('.', '_')
+
+    print(f'Step: {name}')
+    execute_commands([
+        f'mkdir {versioned_dist}',
+        f'cp -r slang {versioned_dist}/'
+        'mkdir ci/release',
+        f'zip -r ci/release/{versioned_dist}.zip {versioned_dist}',
+        # f'tar -zcvf ci/{versioned_dist}.zip {versioned_dist}',
+    ])
